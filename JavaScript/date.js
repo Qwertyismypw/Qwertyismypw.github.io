d9cae1887e408f5eeff32d26695aa6a4dc8b4d69
Qwertyismypw <jshsonntag@yahoo.com>
2016-12-04 22:27:40 -0500

    Create date.js

---
+++ JavaScript/date.js
@@ -0,0 +1,1 @@
+function clock() { var d = new Date(); var day = d.getDate(); var year = d.getFullYear(); var month = d.getMonth(); var hours = d.getHours(); var minutes = d.getMinutes(); var seconds = d.getSeconds();  if (hours > 12){  tod = 'PM'  hours = hours-12 }else{  tod = 'AM' } return month + '-' + day + '-' + year + ';' + hours + ':' + minutes + ':' + seconds + ' ' + tod}
